81dfca57e71cd620513b96eea33fb044e6f30f66
thcpc <redcpc@163.com>
2023-05-30 15:22:32 +0800

    add the new err

--- plugins/wmysql_lib/wmysql_lib_1_0_0/src/wmysql_lib_1_0_0/operate/dump_in.py
+++ plugins/wmysql_lib/wmysql_lib_1_0_0/src/wmysql_lib_1_0_0/operate/dump_in.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 class DumpIn:
-    def __init__(self, mysql_exe, database_info: dict):
+    def __init__(self, *, mysql_exe, database_info: dict):
         self.database_info = database_info
         self.mysql_exe = mysql_exe
         self.cmd_template = "\"{mysql_exe}\" -u{u} -p{p} {dbname} -h{h} < {sql_path}"
@@ -17,8 +17,7 @@ class DumpIn:
                                         dbname=self.database_info.get("database"))
 
     def exec(self, sql_file):
-        if not os.path.exists(sql_file): return {"status": -100, "err": f"no {sql_file}"}
+        if not os.path.exists(sql_file): raise PluginFailErr(f"no {sql_file}")
         ret = subprocess.getstatusoutput(self.command(sql_file))
-        if ret[0] != 0:
-            return {"status": -100, "err": ret[1]}
+        if ret[0] != 0: raise PluginFailErr(ret[1])
         return {"status": 200}
