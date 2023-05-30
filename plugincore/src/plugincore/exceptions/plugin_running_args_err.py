90fac0810ca6bdee91928246913e3433a438034f
thcpc <redcpc@163.com>
2023-05-30 14:52:09 +0800

    new Err Type

---
+++ plugincore/src/plugincore/exceptions/plugin_running_args_err.py
@@ -0,0 +1,5 @@
+class PluginRunningArgsErr(Exception):
+    def __init__(self, *args, **kwargs):
+        self.err = args[0]
+
+    def __call__(self, *args, **kwargs): return dict(status=-200, err=self.err)
