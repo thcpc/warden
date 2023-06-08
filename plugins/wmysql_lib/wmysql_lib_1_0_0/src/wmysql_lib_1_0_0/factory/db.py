d8b2e39be35659a694a86761decb58cf91537186
thcpc <redcpc@163.com>
2023-06-08 19:09:06 +0800

    update the wmysql lib

---
+++ plugins/wmysql_lib/wmysql_lib_1_0_0/src/wmysql_lib_1_0_0/factory/db.py
@@ -0,0 +1,17 @@
+from wmysql_lib_1_0_0.config.db_driver import DBDriver
+from wmysql_lib_1_0_0.support.db_one import DBOne
+from wmysql_lib_1_0_0.support.db_process import DBProcess
+from wmysql_lib_1_0_0.support.db_thread import DBThread
+
+
+def factory(slq_count: int, db_driver: DBDriver):
+    return clz(sql_count=slq_count)(db_driver)
+
+
+def clz(sql_count: int):
+    if sql_count < 10:
+        return DBOne
+    elif sql_count < 60:
+        return DBThread
+    else:
+        return DBProcess
